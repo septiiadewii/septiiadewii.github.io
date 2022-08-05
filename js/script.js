ea5640f277b9ade747add94604912891bc7c7e6f
septia dewi <septiadewi211@gmail.com>
2022-08-05 14:54:46 +0700

    new

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('.nav-link').on('click', function(e) {
+
+    var tujuan = $(this).attr('href');
+   
+    var elemenTujuan = $(tujuan);
+   
+    $('html , body').animate({
+     scrollTop: elemenTujuan.offset().top - 50
+    });
+   
+    e.preventDefault();
+   });
